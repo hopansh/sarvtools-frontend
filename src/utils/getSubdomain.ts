5353464e46129530a662036192f345b6a8ab4a33
hopansh <hopanshgahlot@gmail.com>
2025-05-01 22:00:34 +0530

    subdomain issue fix

--- src/utils/getSubdomain.ts
+++ src/utils/getSubdomain.ts
@@ -1,8 +1,17 @@
+import { siteConfigs } from '@/config/siteConfigs';
+
 // Returns the subdomain from window.location.hostname
 export function getSubdomain(
   hostname: string = window.location.hostname,
 ): string {
-  const parts = hostname.split('.');
-  if (parts.length < 3 && parts[1] !== 'localhost') return 'default';
-  return parts[0];
+  const parts = hostname.split('.').slice(0, 2);
+  const availableSubdomains = Object.keys(siteConfigs);
+  let name = 'default';
+  availableSubdomains.forEach((subdomain) => {
+    if (parts.includes(subdomain)) {
+      name = subdomain;
+      return;
+    }
+  });
+  return name;
 }
